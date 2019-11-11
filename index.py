1817b3aa03fea6d7a2cde36da9ba48b3db150ebb
Alemas <mateusreckziegel@gmail.com>
2019-11-11 00:13:52 -0300

    progress!

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 import os
+import math
+import numpy as np
+from Crypto.Hash import SHA256
 
-def selectFileFromBasepath(basepath = "data") :
+def selectFileFromBasepath(basepath = "data"):
     files = []
     index = 0
     print("Escolha o arquivo a ser usado:")
@@ -14,6 +17,31 @@ def selectFileFromBasepath(basepath = "data") :
     while (option < 0 or option >= index):
         option = int(input("Escolha uma opção válida\n"))
 
-    return open(basepath + "/" + files[option])
+    return open(basepath + "/" + files[option], "rb")
 
-file = selectFileFromBasepath()
+file_bytes = selectFileFromBasepath().read()
+block_size = 1024
+file_blocks = np.array_split(file_bytes, block_size)
+
+print(file_blocks[3:5])
+
+last_block_hash = None
+
+for block in reversed(file_blocks):
+    if len(block) < block_size:
+        last_block_hash = sha256.new(block)
+
+
+# last_block_size = len(file_bytes) % block_size
+# total_blocks = math.ceil(len(file_bytes) / block_size)
+
+# current_block = None
+
+# for i in range(total_blocks, 0, -1):
+#     if current_block == None:
+#         print(i)
+#         block_range = slice(i * block_size, (i * block_size) + last_block_size)
+#         print("range = " + str(block_range))
+#         current_block = file_bytes[block_range]
+
+# print(len(current_block))
